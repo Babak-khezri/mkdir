4ec55face909d38d76843c0ae2ffa2ff227f40ad
babakkhezry <babak.khezri7@gmail.com>
2020-02-21 12:18:40 +0330

    first

---
+++ mkdir.py
@@ -0,0 +1,17 @@
+from os import *
+from random import *
+system("cd ..\..\..")
+name = "abcdefghijklmnopqrstuvwxyz123456789#@!$%^&*()_+=-/\?.,:;><~`"
+le = len(name) - 1
+while True:
+    rand_1 = randint(0,le)
+    rand_2 = randint(0,le)
+    rand_3 = randint(0,le)
+    rand_4 = randint(0,le)
+    rand_5 = randint(0,le)
+    rand_6 = randint(0,le)
+    rand_7 = randint(0,le)
+    rand_8 = randint(0,le)
+    direcory = name[rand_1] + name[rand_2] + name[rand_3] + name[rand_4] + name[rand_5] + name[rand_6] + name[rand_7] + name[rand_8]
+    system("cd ..")
+    system("mkdir {}".format(direcory))
