2830387491c3c245b33dcf2ea4118f614686cec4
babakkhezry <babak.khezri7@gmail.com>
2020-02-26 08:41:28 +0330

    make directory on Desktop

--- mkdir.py
+++ mkdir.py
@@ -1,8 +1,22 @@
 from os import *
+from sys import *
 from random import *
-system("cd ..\..\..")
+def Desktop():
+    lst = []
+    addres = getcwd()
+    l = len(addres)
+    for i in range(l):
+        lst.append(addres[i])
+    addres = ''
+    for i in range(l):
+        addres += lst[i]
+        if lst[i] == 'p' and lst[i-1] == 'o' and lst[i-2] == 't' and lst[i-3] == 'k' and lst[i-4] == 's' and lst[i-5] == 'e' and lst[i-6] == 'D':
+            break            
+    return addres
+addres = Desktop()
+chdir(addres)
 name = "abcdefghijklmnopqrstuvwxyz#@!$%^&*()_+=-/\?.,:;><~`"
-le = len(name) - 2
+le = len(name) - 1
 while True:
     rand_1 = randint(0,le)
     rand_2 = randint(0,le)
